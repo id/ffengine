a2b030c3fedba21590ab8cfd34eacbccbb48652f
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2015-12-13 23:43:46 +0100

    remove unused functions

--- src/ffengine_utils.erl
+++ src/ffengine_utils.erl
@@ -11,11 +11,7 @@
         , id/0
         , utc_time/0
         , time_plus/2
-        , time_to_iso8601/1
         , time_to_url/1
-        , list_to_str/1
-        , tuple_to_map/2
-        , comma_separate/1
         ]).
 
 %%%_* API ======================================================================
@@ -40,37 +36,7 @@ utc_time() ->
 time_plus(DateTime, Seconds) ->
   calendar:gregorian_seconds_to_datetime(calendar:datetime_to_gregorian_seconds(DateTime) + Seconds).
 
-time_to_iso8601({{Year, Month, Day}, {Hour, Minute, Second}}) ->
-  io_lib:format("~4w-~2..0w-~2..0w ~2..0w:~2..0w:~2..0w UTC",
-                [Year, Month, Day, Hour, Minute, Second]).
-
 time_to_url({{Year, Month, Day}, {Hour, Minute, Second}}) ->
   io_lib:format("~4w~2..0w~2..0w_~2..0w~2..0w~2..0w_~5..0w",
                 [Year, Month, Day, Hour, Minute, Second, random:uniform(99999)]).
 
-list_to_str(L) ->
-  string:strip(string:strip(lists:flatten(io_lib:format("~p", [L])), left, $[), right, $]).
-
-tuple_to_map(Fields, Tuple) ->
-  tuple_to_map(Fields, erlang:tuple_to_list(Tuple), #{}).
-
-tuple_to_map([], [], Map) ->
-  Map;
-tuple_to_map([F | Fields], [<<"[null]">> | Values], Map) ->
-  tuple_to_map(Fields, Values, Map#{F => []});
-tuple_to_map([F | Fields], [V | Values], Map) ->
-  tuple_to_map(Fields, Values, Map#{F => V}).
-
-comma_separate([]) ->
-  [];
-comma_separate([_] = L) ->
-  L;
-comma_separate([_|_] = L) ->
-  comma_separate(L, []).
-
-comma_separate([X], Acc) ->
-  [Acc, ",", X];
-comma_separate([X | Tail], []) ->
-  comma_separate(Tail, [X]);
-comma_separate([X | Tail], Acc) ->
-  comma_separate(Tail, [Acc, ",", X]).
