080b315dd5b7a06078e5c3b9fe0b1c32007cd9ab
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2016-01-03 12:03:25 +0100

    another attempt

--- db/init.sh
+++ db/init.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-psql -c "create role ffengine with login password 'ffengine'"; -U postgres
-psql -c "create database ffengine owner ffengine;" -U postgres
-psql -c "grant all on database ffengine to ffengine;" -U postgres
+cat db/roles.sql | sudo su - postgres -c "psql"
 echo "localhost:5432:ffengine:ffengine:ffengine" >> ~/.pgpass
 chmod 0600 ~/.pgpass
-psql -d ffengine -h localhost -U ffengine -f schema.sql
-psql -d ffengine -h localhost -U ffengine -f functions.sql
+psql -d ffengine -h localhost -U ffengine -f db/schema.sql
+psql -d ffengine -h localhost -U ffengine -f db/functions.sql
 
